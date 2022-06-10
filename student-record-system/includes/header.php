b46d447dd0828a11e39647a25b5fa574d3deae10
firdauszainol <firdausz97.my@gmail.com>
2022-06-10 12:41:25 +0800

    update system - testing user create account

--- student-record-system/includes/header.php
+++ student-record-system/includes/header.php
@@ -77,7 +77,8 @@ foreach($results as $result)
            <?php if($_SESSION['login']){?>
             <li><a href="profile.php">Profile Settings</a></li>
               <li><a href="update-password.php">Update Password</a></li>
-            <li><a href="my-booking.php">My Student</a></li>
+              <li><a href="create-student.php">Create Student Account</a></li>
+            <li><a href="my-student.php">My Student</a></li>
             <li><a href="post-testimonial.php">Post a Feedback</a></li>
           <li><a href="my-testimonials.php">My Feedback</a></li>
             <li><a href="logout.php">Sign Out</a></li>
--- student-record-system/includes/sidebar.php
+++ student-record-system/includes/sidebar.php
@@ -2,7 +2,8 @@
           <ul>
             <li><a href="profile.php">Profile Settings</a></li>
               <li><a href="update-password.php">Update Password</a></li>
-            <li><a href="my-booking.php">My Class</a></li>
+              <li><a href="create-student.php">Create Student Account</a></li>
+            <li><a href="my-student.php">My Class</a></li>
             <li><a href="post-testimonial.php">Post a Feedback</a></li>
                <li><a href="my-testimonials.php">My Feedback</a></li>
             <li><a href="logout.php">Sign Out</a></li>
